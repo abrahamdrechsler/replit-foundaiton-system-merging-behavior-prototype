80b63d7d4cfbe7bab0529f736c6375484dba9e63
abrahamdrechsler <99840233+abrahamdrechsler@users.noreply.github.com>
2026-07-07 11:10:27 -0400

    Fix GitHub Pages routing

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: './',
+  base: '/replit-foundaiton-system-merging-behavior-prototype/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
